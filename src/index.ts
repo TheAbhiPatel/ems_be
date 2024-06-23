b2f4ab22afeaf757213637ae5e00014bd490fedc
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-06-23 15:11:42 +0530

    feat: basic express server has been setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Welcome to EMS System");
+import express from "express";
+const app = express();
+
+app.get("/", function (req, res) {
+  res
+    .status(200)
+    .json({ success: true, message: "Welcome to the EMS System." });
+});
+
+app.listen(3331, () => {
+  console.log(`[::] Server is running at http://localhost:3331`);
+});
