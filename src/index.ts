7394d436ad626f97626802b23cb1fb50782c1615
Theabhipatel <theabhipatel.co@gmail.com>
2024-06-29 11:15:08 +0530

    feat registered exress json and urlencoded middlewares

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,23 @@ import router from "./routes";
 
 const app = express();
 
+/** ---> Registering middlewares */
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   res
     .status(200)
     .json({ success: true, message: "Welcome to the EMS System." });
 });
 
+/** ---> Handling all application's routes */
 app.use("/api/v1", router);
+
+/** ---> Handling not found (404) routes */
+app.use("*", (req, res) => {
+  res.status(404).json({ success: false, message: "Route not found." });
+});
 
 app.listen(3331, () => {
   blueLog(`[::] Server is running at http://localhost:3331`);
