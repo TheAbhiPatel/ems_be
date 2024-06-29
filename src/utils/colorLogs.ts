5a9dec35519b9b9e676ba9646f6897803a46dba8
Theabhipatel <theabhipatel.co@gmail.com>
2024-06-29 11:35:30 +0530

    chore update errorLog's log color

--- src/utils/colorLogs.ts
+++ src/utils/colorLogs.ts
@@ -1,9 +1,11 @@
 import colors from "colors";
 
+// eslint-disable-next-line
 export const blueLog = (param: any) => {
   console.log(colors.blue.bold(param));
 };
 
+// eslint-disable-next-line
 export const errorLog = (param: any) => {
-  console.log(colors.magenta.bold("Error :\n"), colors.magenta(param));
+  console.log(colors.magenta.bold("Error :\n"), colors.dim(param));
 };
