819f3aabc1be6831594828621fc2a4f4289aada9
Theabhipatel <theabhipatel.co@gmail.com>
2024-07-02 10:57:18 +0530

    feat created route for login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
-import { registerHandler } from "@cont/auth.controllers";
+import { loginHandler, registerHandler } from "@cont/auth.controllers";
 import { Router } from "express";
 
 const authRouter = Router();
 
 authRouter.post("/register", registerHandler);
+authRouter.post("/login", loginHandler);
 
 export default authRouter;
