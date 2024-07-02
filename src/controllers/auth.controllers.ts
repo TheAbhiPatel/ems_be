bcefff13e4238e208091e2e065dd523f6a80d6ed
Theabhipatel <theabhipatel.co@gmail.com>
2024-07-02 10:54:03 +0530

    feat created register controller with password hashing

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,8 +1,33 @@
 import { RequestHandler } from "express";
+import userModel from "src/models/user.model";
+import bcrypt from "bcryptjs";
+import userProfileModel from "src/models/userProfile.model";
 
-export const loginHandler: RequestHandler = (req, res, next) => {
+export const registerHandler: RequestHandler = async (req, res, next) => {
   try {
-    res.status(200).json({ success: true, message: "Welcome to login route" });
+    const { firstName, lastName, email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (user)
+      return res
+        .status(403)
+        .json({ success: false, message: "User already registered." });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await userModel.create({
+      email,
+      password: hashedPassword
+    });
+    await userProfileModel.create({
+      user: newUser._id,
+      firstName,
+      lastName
+    });
+
+    res
+      .status(201)
+      .json({ success: true, message: "User registered successfully." });
   } catch (error) {
     next(error);
   }
