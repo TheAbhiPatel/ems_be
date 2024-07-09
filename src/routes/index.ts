d84da7f1c3665b0af76fe3f533ff20c35dc47b0e
Theabhipatel <theabhipatel.co@gmail.com>
2024-07-09 11:21:42 +0530

    chore added attendance route in index route file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import authRouter from "./auth.routes";
+import attendanceRouter from "./attendance.routes";
+import { authorize } from "src/middlewares/authorize";
+import { ERoles } from "src/models/user.model";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use(
+  "/attendance",
+  authorize([ERoles.ADMIN, ERoles.MANAGER, ERoles.EMPLOYEE]),
+  attendanceRouter
+);
 
 export default router;
