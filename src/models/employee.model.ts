02bc99422e487d05ff0fe533ff1c6b61f9b785d0
Theabhipatel <theabhipatel.co@gmail.com>
2024-07-10 11:41:45 +0530

    feat created employee schema and model

---
+++ src/models/employee.model.ts
@@ -0,0 +1,58 @@
+import { Document, Schema, model } from "mongoose";
+
+interface IBaseEmployee {
+  user: Schema.Types.ObjectId;
+  userProfile: Schema.Types.ObjectId;
+  company: Schema.Types.ObjectId;
+  position: string;
+  department: string;
+  dateOfJoining: Date;
+  salary: number;
+  isDeleted: boolean;
+}
+
+interface IEmployeeSchema extends Document, IBaseEmployee {}
+
+const employeeSchema = new Schema<IEmployeeSchema>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      index: true
+    },
+    userProfile: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true
+    },
+    company: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    position: {
+      type: String,
+      required: true
+    },
+    department: {
+      type: String,
+      required: true
+    },
+    salary: {
+      type: Number,
+      required: true
+    },
+    dateOfJoining: {
+      type: Date,
+      required: true
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+);
+
+const employeeModel = model<IEmployeeSchema>("employee", employeeSchema);
+export default employeeModel;
