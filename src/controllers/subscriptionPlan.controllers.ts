2e460a8996298de5d201e352e1c96744493e7af6
Theabhipatel <theabhipatel.co@gmail.com>
2024-07-11 11:36:21 +0530

    feat created a controller for subscription plan

---
+++ src/controllers/subscriptionPlan.controllers.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from "express";
+import subscriptionPlanModel from "src/models/subscriptionPlan.model";
+
+export const createSubscriptionPlanHandler: RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const userId = req.user.userId;
+    const { name, description, monthlyPrice, yearlyPrice, features } = req.body;
+
+    await subscriptionPlanModel.create({
+      createdBy: userId,
+      name,
+      description,
+      monthlyPrice,
+      yearlyPrice,
+      features
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Subscription plan successfully created."
+    });
+  } catch (error) {
+    next(error);
+  }
+};
