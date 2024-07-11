decd3fd2871a8c2aec7b33a5226be1856adc329a
Theabhipatel <theabhipatel.co@gmail.com>
2024-07-11 11:38:20 +0530

    chore registered subscription plan routes in index.ts route file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import authRouter from "./auth.routes";
 import attendanceRouter from "./attendance.routes";
 import { authorize } from "src/middlewares/authorize";
 import { ERoles } from "src/models/user.model";
+import subscriptionRouter from "./subscriptionPlan.routes ";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/subscriptions", subscriptionRouter);
 router.use(
   "/attendance",
   authorize([ERoles.ADMIN, ERoles.MANAGER, ERoles.EMPLOYEE]),
