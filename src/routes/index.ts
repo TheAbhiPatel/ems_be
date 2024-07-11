644aaab95117ea05dd8b7198e124b35c2866c24e
Theabhipatel <theabhipatel.co@gmail.com>
2024-07-11 11:42:11 +0530

    chore registered admin routes in index.ts route file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,13 @@ import attendanceRouter from "./attendance.routes";
 import { authorize } from "src/middlewares/authorize";
 import { ERoles } from "src/models/user.model";
 import subscriptionRouter from "./subscriptionPlan.routes ";
+import adminRouter from "./admin.routes";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/subscriptions", subscriptionRouter);
+router.use("/admin", adminRouter);
 router.use(
   "/attendance",
   authorize([ERoles.ADMIN, ERoles.MANAGER, ERoles.EMPLOYEE]),
